706b6f58d97f42785db62f0cc4e0298d2c6baf8f
Andrew Clark <acdlite@me.com>
2015-09-01 15:51:23 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/**
+ * Set the displayName of components created with react-pure-component
+ */
+export default function ({ Plugin, types: t }) {
+  return new Plugin('pure-component', {
+    visitor: {
+      VariableDeclarator(node) {
+        if (
+          node.init.type === 'CallExpression' &&
+          node.init.callee.type === 'Identifier' &&
+          node.init.callee.name === 'pure'
+        ) {
+          const displayName = node.id.name;
+          return [
+            node,
+            t.expressionStatement(
+              t.assignmentExpression(
+                '=',
+                t.memberExpression(
+                  t.identifier(displayName),
+                  t.identifier('displayName')
+                ),
+                t.literal(displayName)
+              )
+            )
+          ];
+        }
+
+        return node;
+      }
+    }
+  });
+}
